fecf6d6446ce019786d4abec1d3815b2161e947d
Enes Tunahan <enes.comez@yaz.com.tr>
2022-06-28 23:06:17 +0300

    Ef Core için gerekli paketler projeye dahil edildi , db bağlantısı kuruldu , DbContext nesnesi oluşturuldu

--- MediumEfCoreRelationships/Program.cs
+++ MediumEfCoreRelationships/Program.cs
@@ -1,6 +1,14 @@
+using MediumEfCoreRelationships.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
